72e3abb538cd406da058e295ed4cd8da65886365
eddableheath <edable-heath@turing.ac.uk>
2025-01-28 08:57:37 +0000

    :hammer::sparkle: feature updates on inpainting

--- src/arc_phash/image_manipulation/deepfake/inpaint.py
+++ src/arc_phash/image_manipulation/deepfake/inpaint.py
@@ -11,7 +11,11 @@ import torch
 from diffusers import AutoPipelineForInpainting
 from PIL.Image import Image
 
-from arc_phash.image_manipulation.deepfake.utils import generate_random_prompt
+from arc_phash.image_manipulation.deepfake.utils import (
+    ANIMALS,
+    KEYWORDS,
+    generate_random_prompt,
+)
 from arc_phash.image_manipulation.utils import resize_and_crop
 
 
@@ -25,6 +29,8 @@ class InPainter:
         seed: int = 37,
         resize_size: int = 1024,
         prompt: str | None = None,
+        save_path: str = "results",
+        img_name: str = "",
         **kwargs,
     ):
         # pipeline
@@ -43,18 +49,24 @@ class InPainter:
         self.results = []
 
         # pipeline kwargs
-        prompt = prompt if prompt is not None else generate_random_prompt("test")
-        # NB: refactor when written
-
+        prompt = (
+            prompt
+            if prompt is not None
+            else generate_random_prompt(keywords=KEYWORDS, animals=ANIMALS, seed=seed)
+        )
         self.pipe_kwargs = {
             "prompt": prompt,
             "device": self.device,
-            "guidance_scale": kwargs.get(4.5),
-            "strength": kwargs.get(self.rng.uniform(0.5, 0.89)),
-            "num_inference_steps": kwargs.get(100),
-            "safety_checker": kwargs.get(),
+            "guidance_scale": kwargs.get("guidance_scale", 4.5),
+            "strength": kwargs.get("strength", self.rng.uniform(0.5, 0.89)),
+            "num_inference_steps": kwargs.get("num_inference_steps", 100),
+            "safety_checker": kwargs.get("safety_checker"),
             "generator": torch.Generator(device=device),
         }
+
+        # save path
+        self.save_path = save_path
+        self.img_name = img_name
 
     def generate_random_mask(self, image: Image) -> Image:
         """Generate a random mask for for an image.
@@ -89,7 +101,7 @@ class InPainter:
         """
         return resize_and_crop(image, target_size=self.resize_size)
 
-    def generate_inpainting(self, image: Image, mask: Image = None):
+    def _generate_inpainting(self, image: Image, mask: Image = None):
         """Generate inpainting based on a mask, with a randomly generated mask if none
         is given
 
@@ -107,6 +119,30 @@ class InPainter:
             {"original": image, "inpainted": inpaint_image, "mask": mask}
         )
 
+    def generate_inpainting(
+        self, image: Image | list[dict[str, Image]], mask: Image = None
+    ):
+        """Generate inpainting on either a single input, or on a list of inputs with
+        dictionary structure:
+                {'image': image, 'mask': mask | None}
+
+        Args:
+            image: Either image or list of image or (image, mask) pairs for inpainting.
+            mask: Mask for inpainting for single image, will gen. Defaults to None.
+        """
+        if not isinstance(image, list):
+            self._generate_inpainting(image=image, mask=mask)
+        else:
+            for img in image:
+                self._generate_inpainting(image=img["image"], mask=img.get("mask"))
+
     def get_results(self):
         """View the results"""
         return self.results
+
+    def save_results(self):
+        """Save the results"""
+        for result in self.results:
+            rand_idx = self.rng.integers(0, 9000)
+            save_dest = f"{self.save_path}/{self.img_name}_inpaint_{rand_idx}.png"
+            result["inpainted"].save(save_dest)
--- src/arc_phash/image_manipulation/deepfake/utils.py
+++ src/arc_phash/image_manipulation/deepfake/utils.py
@@ -2,14 +2,59 @@
     Utility functions for deep fake image manipulation
 """
 
+import numpy as np
+from wonderwords import RandomSentence
 
-def generate_random_prompt(test: bool = False) -> str:
+KEYWORDS = ["roam", "grungy", "journal", "drive", "ridge"]
+ANIMALS = [
+    "man",
+    "woman",
+    "cat",
+    "dog",
+    "bird",
+    "fish",
+    "elephant",
+    "giraffe",
+    "lion",
+    "tiger",
+    "bear",
+    "wolf",
+    "fox",
+    "rabbit",
+    "deer",
+    "moose",
+    "squirrel",
+    "chipmunk",
+    "beaver",
+    "otter",
+    "raccoon",
+    "skunk",
+]
+
+
+def generate_random_prompt(
+    keywords: list[str],
+    animals: list[str],
+    test: bool = False,
+    seed: int = 37,
+) -> str:
     """Generate a random prompt for use in AI image manipulation.
 
     Args:
         test: Optionally return test string. Defaults to False.
+        seed: Seed for randomness. Defaults to 37.
+        keywords: list of keywords for prompt generation.
+        animals: list of animals for prompt generation.
+
+    Returns:
+        prompt, str
     """
     if test:
         return "A cat holding a sign that says hello world"
 
-    return NotImplementedError
+    rng = np.random.default_rng(seed=seed)
+    sentence_tool = RandomSentence()
+
+    keyword = rng.choice(keywords)
+    animal = rng.choice(animals)
+    return f"{keyword} {animal} {sentence_tool.bare_bone_with_adjective()}"
