5c6bbc245d508b0d55e7078f8215e16955d002e5
J-Dymond <jackdymond@me.com>
2025-03-12 23:30:26 +0000

    inpainting script works to generate impainted data

--- src/arc_phash/data_generation/perform_inpainting.py
+++ src/arc_phash/data_generation/perform_inpainting.py
@@ -3,66 +3,66 @@ import os
 
 import torch
 from diffusers import AutoPipelineForInpainting
-from diffusers.utils import load_image, make_image_grid
-from torchvision import transforms
+from diffusers.utils import load_image
 
-pipeline = AutoPipelineForInpainting.from_pretrained(
-    "runwayml/stable-diffusion-inpainting",
-    device="mps",
-)
+# from PIL import Image
+from tqdm import tqdm
 
 generator = torch.Generator("mps").manual_seed(92)
 
+prompts = {
+    "people": (
+        "Change the face of this person, so that it looks like a different person. "
+        "It should be realistic and high quality."
+    ),
+    "children": (
+        "Change the face of this child, so that it looks like a different child. "
+        "It should be realistic and high quality."
+    ),
+    "animals": (
+        "Change the face of this animal, so that it looks like a different animal. "
+        "It should be realistic and high quality."
+    ),
+    "nature": (
+        "Change this object so that it looks like a different object. "
+        "It should be realistic and high quality."
+    ),
+}
 
-def perform_inpainting(
-    image_path, mask_path
-):  # , model_name="facebook/dpt-inpainting"):
-    # Load the image and mask
-    # image = Image.open(image_path).convert("RGB")
-    # mask = Image.open(mask_path).convert("L")
 
+def perform_inpainting(image_path, mask_path, pipeline, prompt=None):
     # load base and mask image
-    init_image = load_image(image_path)
-    mask_image = load_image(mask_path)
-    # Convert PIL images to PyTorch tensors and move to MPS device
-    transform = transforms.ToTensor()
-    init_image = transform(init_image).unsqueeze(0).to("mps")
-    mask_image = transform(mask_image).unsqueeze(0).to("mps")
+    init_image = load_image(image_path).convert("RGB")
+    mask_image = load_image(mask_path).convert("L")
 
-    prompt = "make random changes to this image"
-    image = pipeline(
+    # width, height = init_image.size
+    # # for stabilityai diffusion model, the width and height should be multiples of 8
+    # width = max(1024, width - (width % 8))
+    # height = max(1024, height - (height % 8))
+
+    return pipeline(
         prompt=prompt,
         image=init_image,
         mask_image=mask_image,
         generator=generator,
+        # width=width,
+        # height=height,
     ).images[0]
-    make_image_grid([init_image, mask_image, image], rows=1, cols=3)
-    # Save the inpainted image
-    return image
-    # # Load the model and processor
-    # model = DPTForImageInpainting.from_pretrained(model_name)
-    # processor = DPTImageProcessor.from_pretrained(model_name)
-
-    # # Preprocess the image and mask
-    # inputs = processor(images=image, masks=mask, return_tensors="pt")
-
-    # # Perform inpainting
-    # with torch.no_grad():
-    #     outputs = model(**inputs)
-
-    # # Get the inpainted image
-    # inpainted_image = processor.post_process(outputs.logits, inputs["pixel_values"])
-
-    # # Convert tensor to PIL image
-    # return Image.fromarray(inpainted_image.squeeze().permute(1, 2, 0).byte().numpy())
 
 
 # Example usage
 def main(args):
+    model_name = args.model_name
     data = args.data
+
     image_dir = f"../data/{data}/images/"
     mask_dir = f"../data/{data}/masks/"
-    inpainted_dir = f"../data/{data}/inpainted/"
+    inpainted_dir = f"../data/{data}/{model_name.replace('/', '-')}/"
+
+    pipeline = AutoPipelineForInpainting.from_pretrained(model_name)
+    pipeline.to("mps")
+    # pipeline.set_progress_bar_config(disable=True)
+
     os.makedirs(inpainted_dir, exist_ok=True)
 
     # Find the first image in the image directory
@@ -73,19 +73,32 @@ def main(args):
         error_str = f"No images found in {image_dir}"
         raise ValueError(error_str)
 
-    image_filename = image_files[0]
-    print(f"First image filename: {image_filename}")
+    for image_filename in tqdm(image_files):
+        if image_filename.endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif")):
+            print(f"First image filename: {image_filename}")
 
-    image_path = os.path.join(image_dir, image_filename)
-    mask_path = os.path.join(mask_dir, image_filename)
-    inpainted_path = os.path.join(inpainted_dir, image_filename)
+            image_path = os.path.join(image_dir, image_filename)
+            mask_path = os.path.join(mask_dir, image_filename)
+            inpainted_path = os.path.join(inpainted_dir, image_filename)
 
-    inpainted_image = perform_inpainting(image_path, mask_path)
-    inpainted_image.save(inpainted_path)
+            inpainted_image = perform_inpainting(
+                image_path, mask_path, pipeline, prompt=prompts[data]
+            )
+            inpainted_image.save(inpainted_path)
 
 
 if __name__ == "__main__":
+    """
+    Example usage:
+    python perform_inpainting.py people "runwayml/stable-diffusion-inpainting
+
+    model_names:
+        -   "runwayml/stable-diffusion-inpainting" (This one actually works)
+        -   "kandinsky-community/kandinsky-2-2-decoder-inpaint" (This one doesn't)
+        -   "stabilityai/stable-diffusion-2-inpainting" (Neither does this one)
+    """
     parser = argparse.ArgumentParser(description="Draw masks on images.")
     parser.add_argument("data", type=str, help="The data type name.")
+    parser.add_argument("model_name", type=str, help="The model to use for inpainting.")
     args = parser.parse_args()
     main(args)
